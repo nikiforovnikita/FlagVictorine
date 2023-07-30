b5c490930e85b1af9fd0df07179c561096d1a133
Nikita <nikiforov2312@gmail.com>
2023-07-30 20:40:23 +0300

    Initial Commit

---
+++ FlagVictorine/FlagVictorineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagVictorineApp.swift
+//  FlagVictorine
+//
+//  Created by MacBook on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagVictorineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
